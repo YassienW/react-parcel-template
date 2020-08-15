505f3cd21484d72db04bb452c3bd859039a655bb
Yassien <11986226+YassienW@users.noreply.github.com>
2020-08-15 13:40:18 +0200

    Update dependencies and clean scripts
    - Update to parcel 2

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import {hot} from "react-hot-loader";
 import App from "./App";
 import "../../res/style.scss";
 
@@ -8,5 +7,4 @@ if (process.env.NODE_ENV !== "production"){
     require("../mock/Mirage").startMirage();
 }
 
-const HotApp = hot(module)(App);
-ReactDOM.render(<HotApp/>, document.getElementById("app"));
+ReactDOM.render(<App/>, document.getElementById("app"));
